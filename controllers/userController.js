b51514668d17737bc190c3021b7f5c720eb4a762
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-28 13:45:13 -0400

    added TODO comments to thoughtController.js and userController.js files following challenge instructions to guide code

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+// TODO: Import Dependencies (import models and mongoose)
+
+
+// ------------------------------------------
+// TODO: GET ALL USERS --> '/'
+
+
+// TODO: GET A SINGLE USER by its '_id' with populated thought and friend data --> '/:userId'
+
+
+// TODO: CREATE A NEW USER --> '/'
+
+
+// TODO: UPDATE USER by its '_id' --> '/:userId'
+
+
+// TODO: DELETE USER by its '_id' --> '/:userId'
+// BONUS: REMOVE a user's associated thoughts when deleted 
+
+
+
+
+
+// ------------------------------------------
+// TODO: ADD A NEW FRIEND to a user's friend list --> '/:userId/friends/:friendId'
+
+
+// TODO: REMOVE A FRIEND from a user's friend list --> '/:userId/friends/:friendId'
+
+
+
+
+// ------------------------------------------
+// TODO: Export
